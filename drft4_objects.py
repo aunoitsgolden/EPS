49660f1f4ac40135c0349be12fa44cd457f0e268
Rowan Golden <38592375+rowangolden@users.noreply.github.com>
2023-06-21 13:11:40 -0400

    drft4_objects.py

--- drft4_objects.py
+++ drft4_objects.py
@@ -8,70 +8,55 @@ import re
 # Use classes and basic OOP to create reusable items to encourage development onto
 # other websites.
 
-class query:
-    def __init__(self, item):
-        self.y = item
-
-class cost(query):
-    def __init__(self, item, min, max):
-        self.x = min
-        self.z = max
-        
-        query().__init__(item) # you can also use super().
-
+#--------------Query--------------
 item = str(input("Product Name:"))
-min, max = input("Please enter the min and max:") # making this (int) crashes the program... I don't know why
+minprice = input("MinPrice:")
+maxprice = input("MaxPrice:")
 
-while not min.isdigit() and max.isdigit():
-    min, max = (input("Please enter a valid number:"))
-while min == 0:
-    min = input("Please enter a value greater than 0 for your minimum:")
-while max > min:
-    max = input("Please input a maximum value greater than the minimum price:")
+# Error Handling
+while not minprice.isdigit() and maxprice.isdigit(): 
+    minprice, maxprice = (input("Please enter a valid number:"))
+while minprice == 0:
+    minprice = input("Please enter a value greater than 0 for your minimum:")
+while maxprice > minprice:
+    maxprice = input("Please input a maximum value greater than the minimum price:")
 
-print("")
-
-
-    # def dictionary(self):
-    #     items_found[self.y] = item
-
+items_found = {}
 
 #-------------------Scraper---------------------
 class WebScraper:
-
-    def __init__(self, url): # initalizing attribute of the WebScraper Class; takes 'url' as an argument and assigns it to 'url' attribute of the class
+    def __init__(self, url, query):
         self.url = url 
+        self.query = query
     
-    def parse_page(self, page, doc):
-        page = requests.get(self.url).text # gather page with GET request
-        doc = BeautifulSoup(page, 'html.parser') # takes a string of HTML content and parses into a format easier to work with
-        return doc # returns BeautifulSoup object
+    def fetch_page(self):
+        raise NotImplementedError("Method will be implemented in subclass.")
+            
+    def parse_page(self, html):
+        doc = BeautifulSoup(html, 'html.parser') 
+        return doc 
 
-    def scrape(self): # uses parse_page to: 
-        page_html = self.fetch_page() # fetches HTML content 
-        parsed_page = self.parse_page(page_html) # parses the content into a BS object
-        return parsed_page # object Returned
+    def scrape(self): 
+        page_html = self.fetch_page() 
+        parsed_page = self.parse_page(page_html) 
+        return parsed_page 
 
-#---------------URLs---------------
-# url = f"https://www.newegg.com/p/pl?N=4131&d={search_term}&LeftPriceRange={minimum_price}+{maximum_price}"
+#----Newegg Scraper----
+class NeweggScraper(WebScraper):
+    def fetch_page(self, page):
+        full_url = f"https://www.newegg.com/p/pl?N=4131&d={item}&LeftPriceRange={minprice}+{maxprice}"
+        response = requests.get(full_url)
+        return response.text 
+        
+        page_text = doc.find(class_="list-tool-pagination-text").strong # We could get rid of this, however currently keeping for possible future use.
+        pages = int(str(page_text).split("/")[-2].split(">")[-1][:-1])
 
-#---- Page Scraping Example ----
-# scraper = WebScraper(url)
-# parsed_page = scraper.scrape()
+        for page in range(1, pages + 1): # +1 because naturally range begins at 0 (but our pages do not)  
+            full_url = f"https://www.newegg.com/p/pl?N=4131&d={item}&LeftPriceRange={minprice}+{maxprice}&page={page}"
+            response = requests.get(full_url)
+            return response.text 
+        
+# Creates instance
+ScraperResults = NeweggScraper.parsed_page
+print(ScraperResults)
 
-# print(parsed_page)
-
-#------------------Dictionary----------------------
-
-items_found = {} # items are being placed into this dictionary
-
-#----------------------------Newegg----------------------------
-# page_text = doc.find(class_="list-tool-pagination-text").strong
-# pages = int(str(page_text).split("/")[-2].split(">")[-1][:-1])
-
-# for page in range(1, pages + 1): # +1 because naturally range begins at 0 (but our pages do not)
-    # url = f"https://www.newegg.com/p/pl?N=4131&d={search_term}&LeftPriceRange={minimum_price}+{maximum_price}&page={page}"
-    # page = requests.get(url).text
-    # doc = BeautifulSoup(page, "html.parser")
-
-#   6
