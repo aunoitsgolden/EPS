a7d85baf5e934198c3be4ee21d518fcf40dd9d3a
Rowan Golden <38592375+rowangolden@users.noreply.github.com>
2023-07-05 14:50:33 -0400

    Update drft4_objects.py
    - Works when searching for multiple words 
    (Does this by omitting prepositions from search query)
    
    - Only works on Newegg
    - Only works on page 1.

--- drft4_objects.py
+++ drft4_objects.py
@@ -1,8 +1,5 @@
 from bs4 import BeautifulSoup
 import requests
-import re
-
-items_found = {}
 
 class Scraper:
     def __init__(self, url):
@@ -14,76 +11,107 @@ class Scraper:
         doc = BeautifulSoup(html.text, 'html.parser')    
         return doc
     
-    def items(self, doc):
+    def search(self, doc): # We can probably remove these classes
         raise NotImplementedError()
-
+        
+    def gather_data(self, matches):
+        raise NotImplementedError()
+        
 class Newegg(Scraper):
-    def items(self, doc):
-        item_container = doc.find(class_="item-cells-wrap border-cells items-grid-view four-cells expulsion-one-cell")
-        items = item_container.find_all(string=re.compile(product))
-        for item in items:
-            parent = item.parent
-            if parent.name != "a":
-                continue
+    def search(self, doc):
+        matches = {}
+        grids = doc.find_all(class_= "item-cells-wrap border-cells items-grid-view four-cells expulsion-one-cell")
+        for grid in grids:
+            items = grid.find_all(class_= "item-cell")
+            for item in items:
+                name_html = item.find('a', {'class': 'item-title'})
+                if name_html: 
+                    name_str = str(name_html)
+                    name = name_html.text
+                    if all(word.lower() in name_str.lower() for word in important_words):
+                        matches[name] = {'item': item}
+                else:
+                    name = ("Title 'item-title' Tag Not Available")
+        return matches
+                    
+    def gather_data(self, matches):
+        for name, data in matches.items():
+            item = data['item']
+            price_current = item.find('li', {'class': 'price-current'})
+            if price_current:
+                price = price_current.find('strong')
+                if price:
+                    price = price.text
+                    try: 
+                        price_float = price_current.find('sup')
+                        if price_float:
+                            price += price_float.text
+                        else: 
+                            pass
+                    except:
+                        price = "Decimal 'sup' Tag Not Available. Please flag this query so we can add this feature."
+                else:
+                    price = "Price 'strong' Tag Not Available. Please flag this query so we can add this feature."
+            else:
+                price = "Price Not Available. 'price-current' HTML class not found. Please flag this query so we can add this feature."
 
-            link = parent['href']
-            next_parent = item.find_parent(class_="item-container")
-            try:    
-                price = next_parent.find(class_="price-current").find('strong').string
-                items_found[item] = {"price": int(price.replace(",","")), "link": link}
-            except:
-                pass
+            link_tag = item.find('a', {'class': 'item-title'})
+            if link_tag and 'href' in link_tag.attrs:
+                link = link_tag['href']
+            else:
+                link = ("Link Not Available. 'item-title' HTML class not found.")
 
-class HomeDepot(Scraper):
-    def items(self, doc):
-        grid = doc.find(class_="results-wrapped")
-        item_container = grid.find_all(id=(re.compile("browse-search-pods"))) # done this way because I have not found a way to access containers individually
-        for items in item_container:
-            details = items.find_all(class_="product-pod--s5vy1") # bs4 only searches the first container (browser-search-pods1) and no other.
-                
-class BestBuy(Scraper):
-    def items(self, doc):
-        list = doc.find(class_="sku-item-list")
-        items = list.find_all(string=re.compile(product))
-        print(items)
+            matches[name] = {"Price": price, "Link": link, "item": item}
+            del matches[name]['item']
+    
+def price_ascending(matches): 
+    return sorted(matches.items(), key=lambda x: float(x[1]['Price']))
+def price_descending(matches): 
+    return sorted(matches.items(), key=lambda x: float(x[1]['Price']), reverse = True)
 
-select = input("What site would you like to search? Newegg, HomeDepot, or BestBuy? ")
-select = select.strip().lower()
+sort_items = {
+        "1": price_ascending,
+        "2": price_descending,}
+
+site_select = (input("What site would you like to search? Newegg, HomeDepot, or BestBuy? ")).strip().lower()
 
 product = str(input("What item would you like to search for? "))
 product_words = product.split()
 
-minprice = input("MinPrice: ") 
+# Options: Input Product and Attributes separately, separate by preposition, exclude preposition.
+ignore_words = ["for", "and", "the", "an", "a"]
+important_words = []
+for word in product_words:
+    if word.lower() not in ignore_words: 
+        important_words.append(word)
+        
+minprice = input("MinPrice: ") # We have to put in some error handling for this
 maxprice = input("MaxPrice: ")
 
-# sort = input("Would you like to sort by price or seller?")
-
-inp_format1 = "%20".join(product_words) if len(product_words) > 1 else product
 inp_format2 = "+".join(product_words) if len(product_words) > 1 else product
 class_selection = {
     "newegg": Newegg(f"https://www.newegg.com/p/pl?N=4131&d={inp_format2}&LeftPriceRange={minprice}+{maxprice}"),
-    "homedepot": HomeDepot(f"https://www.homedepot.com/s/{inp_format1}?NCNI-5&lowerBound={minprice}&upperBound={maxprice}"),
-    "bestbuy": BestBuy(f"https://www.bestbuy.com/site/searchpage.jsp?st={inp_format2}&qp=currentprice_facet%3DPrice~{minprice}%20to%20{maxprice}"),
 } # Need to add page loop
 
-selected_class = class_selection.get(select)
-if selected_class:
+selected_class = class_selection.get(site_select)
+if selected_class: 
     instance = selected_class
-else:
-    print("The scraper for Site {site} hasn't been implemented yet.")
+else: 
+    print("The scraper for that site hasn't been implemented yet.") 
 
 doc = instance.fetch_page()
-instance.items(doc)
+matches = instance.search(doc)
+instance.gather_data(matches)
 
-# sorted_items = sorted(items_found.items(), key=lambda x: x[1]['price'])
+sort_select = input("How would you like to sort your items? (1): Low to High (2): High to Low ") # (3): Seller, A to Z (4): Seller, Z to A 
 
-# print(sorted_items)
-
-# sorted = sorted(items_found.items(), key=lambda x: x[1]['seller'])
-# sort_option = sort_select.get(items_found)
-# if sort_option:
-#     instance = sort_option
-# else:
-#     print("We cannot sort by that yet.")
-
-
+sort_function = sort_items.get(sort_select)
+if sort_function:
+    sorted_items = sort_function(matches) 
+    for item in sorted_items:
+        print(f"${item[1]['Price']}") 
+        print(item[0])
+        print(item[1]['Link']) 
+        print("----------------------------------------------")
+else: 
+    print("That kind of sorting hasn't been implemented yet.") 
